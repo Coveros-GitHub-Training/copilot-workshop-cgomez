55d28e6f0a14362068056447b30b42405e7c8f18
Carlos Gomez <cgomez@mib.com>
2026-01-26 17:07:06 -0500

    Add the Featured Recipes section

--- flavorhub/src/main/java/com/coveros/training/flavorhub/controller/HomeController.java
+++ flavorhub/src/main/java/com/coveros/training/flavorhub/controller/HomeController.java
@@ -1,16 +1,37 @@
 package com.coveros.training.flavorhub.controller;
 
+import com.coveros.training.flavorhub.model.Recipe;
+import com.coveros.training.flavorhub.service.RecipeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Controller for serving the main web pages
  */
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
     
+    private final RecipeService recipeService;
+    
+    /**
+     * Display the home page with featured recipes
+     * @param model the model to add attributes to
+     * @return the index view template
+     */
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        // Get featured recipes (limit to 6 for the home page)
+        List<Recipe> allRecipes = recipeService.getAllRecipes();
+        List<Recipe> featuredRecipes = allRecipes.stream()
+                .limit(6)
+                .toList();
+        
+        model.addAttribute("featuredRecipes", featuredRecipes);
         return "index";
     }
     
